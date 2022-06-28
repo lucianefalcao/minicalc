af82549138552fe2d0d4ef8df6ea2de4a0c887df
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-06-28 05:41:09 -0300

    cria novos tokens e define tamanho máximo para o nome da variável

--- lexer.h
+++ lexer.h
@@ -5,16 +5,23 @@
 #ifndef MINICALC_LEXER_H
 #define MINICALC_LEXER_H
 
+#define IDENT_TAM_MAX 60
+
 #include <stdbool.h>
 
-typedef enum tagTipoToken {
+typedef enum tagTipoToken
+{
     TOKEN_INT,
     TOKEN_PRINT,
+    TOKEN_VAR,
+    TOKEN_IDENT,
     TOKEN_ERRO,
     TOKEN_SOMA,
     TOKEN_MULT,
     TOKEN_ABREPAR,
     TOKEN_FECHAPAR,
+    TOKEN_PONTOVIRG,
+    TOKEN_IGUAL,
     TOKEN_SUB,
     TOKEN_DIV,
     TOKEN_ABRECOLCH,
@@ -25,18 +32,19 @@ typedef enum tagTipoToken {
     TOKEN_EOF,
 } TipoToken;
 
-typedef struct tagToken {
+typedef struct tagToken
+{
     TipoToken tipo;
-    union {
+    union
+    {
         int valorInt;
         double valorFloat;
     } valor;
+    char nome[IDENT_TAM_MAX];
 } Token;
 
-
 void InicializaLexer(char *arqFonte);
-Token* ProximoToken();
+Token *ProximoToken();
 void FinalizaLexer();
 
-
-#endif //MINICALC_LEXER_H
+#endif // MINICALC_LEXER_H
