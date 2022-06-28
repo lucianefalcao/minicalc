f5d8f1a1933531a56d325cadeafe6d77b54509d3
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-06-28 06:14:22 -0300

    modifica o main para suportar variáveis

--- main.c
+++ main.c
@@ -5,53 +5,68 @@
 #include "arq.h"
 #include "lexer.h"
 #include "parser.h"
+#include "var.h"
 
-int AvaliaExpressao(Expressao* e) {
+int AvaliaExpressao(Expressao *e)
+{
     int res = 0;
     int v1, v2;
 
-    switch (e->oper) {
-        case OPER_CONST:
-            res = e->valor;
-            break;
-        case OPER_SOMA:
-            v1 = AvaliaExpressao(e->op1);
-            v2 = AvaliaExpressao(e->op2);
-            res = v1 + v2;
-            break;
-        case OPER_MULT:
-            v1 = AvaliaExpressao(e->op1);
-            v2 = AvaliaExpressao(e->op2);
-            res = v1 * v2;
-            break;
-        case OPER_SUB:
-            v1 = AvaliaExpressao(e->op1);
-            v2 = AvaliaExpressao(e->op2);
-            res = v1 - v2;
-            break;
-        case OPER_DIV:
-            v1 = AvaliaExpressao(e->op1);
-            v2 = AvaliaExpressao(e->op2);
-            res = v1 / v2;
-            break;
-        case OPER_RESTODIV:
-            v1 = AvaliaExpressao(e->op1);
-            v2 = AvaliaExpressao(e->op2);
-            res = v1 % v2;
-            break;
-        case OPER_EXPO:
-            v1 = AvaliaExpressao(e->op1);
-            v2 = AvaliaExpressao(e->op2);
-            res = (int) powf((float)v1, (float)v2);
-            break;
-        default:
-            printf("Operador nao reconhecido.\n");
+    switch (e->oper)
+    {
+    case OPER_VAR:
+        if (ConsultarVar(e->nomeIdent, &v1))
+        {
+            res = v1;
+        }
+        else
+        {
+            fprintf(stderr, "Variavel nao declarada: %s\n", e->nomeIdent);
+            exit(3);
+        }
+        break;
+    case OPER_CONST:
+        res = e->valor;
+        break;
+    case OPER_SOMA:
+        v1 = AvaliaExpressao(e->op1);
+        v2 = AvaliaExpressao(e->op2);
+        res = v1 + v2;
+        break;
+    case OPER_MULT:
+        v1 = AvaliaExpressao(e->op1);
+        v2 = AvaliaExpressao(e->op2);
+        res = v1 * v2;
+        break;
+    case OPER_SUB:
+        v1 = AvaliaExpressao(e->op1);
+        v2 = AvaliaExpressao(e->op2);
+        res = v1 - v2;
+        break;
+    case OPER_DIV:
+        v1 = AvaliaExpressao(e->op1);
+        v2 = AvaliaExpressao(e->op2);
+        res = v1 / v2;
+        break;
+    case OPER_RESTODIV:
+        v1 = AvaliaExpressao(e->op1);
+        v2 = AvaliaExpressao(e->op2);
+        res = v1 % v2;
+        break;
+    case OPER_EXPO:
+        v1 = AvaliaExpressao(e->op1);
+        v2 = AvaliaExpressao(e->op2);
+        res = (int)powf((float)v1, (float)v2);
+        break;
+    default:
+        printf("Operador nao reconhecido.\n");
     }
 
     return res;
 }
 
-int main() {
+int main()
+{
     InicializaLexer("../test/expcomplexa.mc");
 
     // arvore sintatica do programa
