34da5b0ac9a60509d46bf5ed43e5532005ec34cd
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-06-28 05:51:36 -0300

    defina a estrutura de declaração

--- parser.h
+++ parser.h
@@ -2,29 +2,44 @@
 #ifndef MINICALC_PARSER_H
 #define MINICALC_PARSER_H
 
-typedef enum tagTipoOper {
+#include "lexer.h"
+
+typedef enum tagTipoOper
+{
     OPER_SOMA,
     OPER_MULT,
     OPER_SUB,
     OPER_DIV,
     OPER_RESTODIV,
     OPER_EXPO,
-    OPER_CONST
+    OPER_CONST,
+    OPER_VAR
 } TipoOper;
 
-typedef struct tagExpressao {
+typedef struct tagExpressao
+{
     struct tagExpressao *op1;
     struct tagExpressao *op2;
     int valor;
+    char nomeIdent[IDENT_TAM_MAX];
     TipoOper oper;
 } Expressao;
 
-typedef struct tagPrograma {
+typedef struct tagDeclaracao
+{
+    char nomeIdent[IDENT_TAM_MAX];
+    Expressao *e;
+    struct tagDeclaracao *next;
+} Declaracao;
+
+typedef struct tagPrograma
+{
+    Declaracao *decls;
     Expressao *e;
 } Programa;
 
-Programa* AnalisePrograma();
-Expressao* AnaliseExpressao();
+Programa *AnalisePrograma();
+Expressao *AnaliseExpressao();
 void DestroiPrograma(Programa *p);
 
-#endif //MINICALC_PARSER_H
+#endif // MINICALC_PARSER_H
